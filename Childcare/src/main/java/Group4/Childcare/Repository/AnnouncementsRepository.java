6931bf174b0493c8b5e9c9c522fd65a2a7b8be00
CHEN,CHUN-AN <D1397030@o365.fcu.edu.tw>
2025-11-26 14:47:57 +0800

    修正錯誤

--- Childcare/src/main/java/Group4/Childcare/Repository/AnnouncementsRepository.java
+++ Childcare/src/main/java/Group4/Childcare/Repository/AnnouncementsRepository.java
@@ -4,11 +4,14 @@ import Group4.Childcare.Model.Announcements;
 import Group4.Childcare.DTO.AnnouncementSummaryDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface AnnouncementsRepository extends JpaRepository<Announcements, UUID> {
 
-    @Query("SELECT new Group4.Childcare.DTO.AnnouncementSummaryDTO(a.announcementID,a.title, a.content, a.startDate) FROM Announcements a order by StartDate desc ")
+    @Query("SELECT new Group4.Childcare.DTO.AnnouncementSummaryDTO(a.announcementID, a.title, a.content, a.startDate) " +
+           "FROM Announcements a ORDER BY a.startDate DESC")
     List<AnnouncementSummaryDTO> findSummaryData();
 }
